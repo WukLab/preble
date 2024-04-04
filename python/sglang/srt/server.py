4f9d066bd8531f453a3ac44799229bc934465881
vikranth22446 <vsrivatsa@ucsd.edu>
2024-04-04 18:40:45 +0000

    Update server.py with better error logging

--- python/sglang/srt/server.py
+++ python/sglang/srt/server.py
@@ -563,8 +563,6 @@ def launch_server(server_args: ServerArgs, pipe_finish_writer):
     if router_init_state != "init ok" or detoken_init_state != "init ok":
         proc_router.kill()
         proc_detoken.kill()
-        with open("sglang_error.log", "a+") as f:
-            f.write(f"Failed to initialize the server. router init state: {router_init_state.strip()}")
         logging.error(f"Failed to initialize the server. router init state: {router_init_state.strip()}, detoken init state: {detoken_init_state}")
         print("router init state:", router_init_state)
         print("detoken init state:", detoken_init_state)
