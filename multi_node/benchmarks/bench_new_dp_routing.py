fe591217b78b98ac9d4493f5c90e0a698a42e50c
vikranth22446 <vsrivatsa@ucsd.edu>
2024-04-11 17:02:34 +0000

    Add support for greedy lp policy

--- multi_node/benchmarks/bench_new_dp_routing.py
+++ multi_node/benchmarks/bench_new_dp_routing.py
@@ -1,39 +1,38 @@
-import warnings
 import random
+import warnings
+
 import numpy as np
+
 random.seed(10)
 np.random.seed(10)
 
-import sys
 import os
+import sys
 
 # Add the parent directory of the 'src' directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import time
-
 import datetime
+import time
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-from multi_node_loader import MultiNodeLoader
-from model_runtime_manager import RequestFuncOutput, ModelDetails
-from benchmark_utils import BenchmarkMetrics, MajorExperimentArgs, WorkloadConfig
-from benchmarks.exp_configs.react_simulator_config import exp_args
-from benchmark_workload_gen import *
-from data_parallel_request_cache import DataParallelRuntimeSelectionPolicy, CustomPolicyType
-from metrics_based_scheduler import LongestPrefixMatchSelector, GlobalLongestPrefixMatch
-from global_policy_lp import LPScheduler
-
-import logging
-import torch
 import gc
-import requests as session
-import argparse
-import re
-from typing import List, Dict
-import paramiko
-import importlib.util
+import logging
+from typing import Dict, List
+import torch
+from data_parallel_request_cache import (
+    CustomPolicyType,
+    DataParallelRuntimeSelectionPolicy,
+)
+from global_policy_lp import LPScheduler
+from greedy_lp import GurobiGreedyLPScheduler
+from metrics_based_scheduler import GlobalLongestPrefixMatch, LongestPrefixMatchSelector
+from model_runtime_manager import ModelDetails, RequestFuncOutput
+from multi_node_loader import MultiNodeLoader
 
+from benchmark_utils import BenchmarkMetrics, MajorExperimentArgs, WorkloadConfig
+from benchmark_workload_gen import *
+from benchmarks.exp_configs.react_simulator_config import exp_args
 
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
@@ -43,11 +42,14 @@ logging.getLogger("paramiko").setLevel(logging.WARNING)
 
 log = logging.getLogger(__name__)
 
-def regist_selector(policy, custom_policy, model_details: ModelDetails, workload_config: WorkloadConfig):
+
+def regist_selector(
+    policy, custom_policy, model_details: ModelDetails, workload_config: WorkloadConfig
+):
     if policy == DataParallelRuntimeSelectionPolicy.CUSTOM:
         if custom_policy == CustomPolicyType.ORACLE:
             oracle = Oracle(
-                num_nodes=len(model_details.runtimes), 
+                num_nodes=len(model_details.runtimes),
                 num_workloads=workload_config.num_prefix_patterns,
             )
             model_details.update_runtime_selection_policy(
@@ -56,7 +58,7 @@ def regist_selector(policy, custom_policy, model_details: ModelDetails, workload
             )
         if custom_policy == CustomPolicyType.ORACLE_HOT_COLD:
             oracle = OracleHotCold(
-                num_nodes=len(model_details.runtimes), 
+                num_nodes=len(model_details.runtimes),
                 num_workloads=workload_config.num_prefix_patterns,
             )
             model_details.update_runtime_selection_policy(
@@ -98,19 +100,22 @@ def regist_selector(policy, custom_policy, model_details: ModelDetails, workload
                 DataParallelRuntimeSelectionPolicy.CUSTOM,
                 custom_runtime_selector=glpm,
             )
-        elif custom_policy == CustomPolicyType.LP_SCHEDULER:
-            lp_scheduler = LPScheduler(num_nodes=len(model_details.runtimes), depth_limit=4, update_interval=5)
+        elif custom_policy == CustomPolicyType.GREEDY_LP:
+            greedy_lp = GurobiGreedyLPScheduler(num_nodes=len(model_details.runtimes))
             model_details.update_runtime_selection_policy(
                 DataParallelRuntimeSelectionPolicy.CUSTOM,
-                custom_runtime_selector=lp_scheduler,
+                custom_runtime_selector=greedy_lp,
             )
     else:
         model_details.update_runtime_selection_policy(policy)
 
+
 def load_and_run_benchmark(
-    model_details: ModelDetails, 
+    model_details: ModelDetails,
     workload_config: WorkloadConfig,
-    policy, custom_policy=None, custom_msg="",
+    policy,
+    custom_policy=None,
+    custom_msg="",
 ):
     num_workloads = workload_config.num_prefix_patterns
     distribution_of_non_shared = workload_config.random_ratio
@@ -119,18 +124,18 @@ def load_and_run_benchmark(
     exp_time = workload_config.exp_time
     requests = workload_config.requests
     tokenizer = workload_config.dataloader.tokenizer
-        
+
     logging.info(
         f"=====STARTING Policy {policy}-{custom_policy}:{custom_msg}, {num_workloads} WORKLOADS, {distribution_of_non_shared} NON-SHARED, {num_requests} REQUESTS, {rps} REQ/s, {exp_time} seconds ====="
     )
     regist_selector(policy, custom_policy, model_details, workload_config)
-    
+
     tic_benchmark = time.time()
     results: List[RequestFuncOutput] = model_details.get_experiment_results(
         requests, rps, exp_time
     )
     overall_latency = time.time() - tic_benchmark
-    
+
     counts = model_details.request_router.get_model_selection_counts()
     bench_metrics = BenchmarkMetrics.gen_benchmark_metrics(
         tokenizer=tokenizer,
@@ -147,10 +152,14 @@ def test_oracle_random_basic(exp_args: MajorExperimentArgs):
     loader = MultiNodeLoader(exp_args.simulate)
     for workload_config in exp_args.workload_configs:
         logging.info(workload_config)
-        logging.info(f"Using load distribution of {workload_config.dataloader.load_dist}")
+        logging.info(
+            f"Using load distribution of {workload_config.dataloader.load_dist}"
+        )
         # dataloader = RandomDataLoader(num_workloads, num_requests, tokenizer, LoadDistribution.EVEN, distribution_of_non_shared, 1)
         for selector_config in exp_args.selector_configs:
-            model_details = loader.load_model(**exp_args.runtime_args) # TODO: clear cache instead of reload
+            model_details = loader.load_model(
+                **exp_args.runtime_args
+            )  # TODO: clear cache instead of reload
             load_and_run_benchmark(model_details, workload_config, *selector_config)
             loader.unload_model(model_details)
             torch.cuda.empty_cache()
@@ -159,6 +168,8 @@ def test_oracle_random_basic(exp_args: MajorExperimentArgs):
 
 
 if __name__ == "__main__":
+    from exp_configs.react_simulator_config_greedy import exp_args
+
     logging.basicConfig(level=logging.INFO, filename=exp_args.log_file_path)
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
     # Add current time to log file
@@ -167,7 +178,7 @@ if __name__ == "__main__":
     logging.info(f"Starting Experiment at {start_date}")
     # model_name = "mistralai/Mistral-7B-v0.1"
     # model_name = "lmsys/vicuna-13b-v1.5"
-    model_name = exp_args.runtime_args['model_path']
+    model_name = exp_args.runtime_args["model_path"]
     logging.info(f"Model Name: {model_name}")
 
     test_oracle_random_basic(exp_args)
---
+++ multi_node/benchmarks/exp_configs/react_simulator_config_greedy.py
@@ -0,0 +1,72 @@
+from transformers import AutoTokenizer
+import random
+import sys, os
+
+# Add the parent directory of the 'src' directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from benchmark_utils import WorkloadConfig, MajorExperimentArgs
+from benchmark_workload_gen import *
+from sglang.srt.managers.router.model_runner import GPUConfig
+from data_parallel_request_cache import DataParallelRuntimeSelectionPolicy, CustomPolicyType
+from exp_configs.exp_config_utils import create_workload_prefix_configs
+from exp_configs.react_simulator_config import add_simulation_to_gpu_config
+
+# -----------------------------------------------------------------------------
+# Helper Functions
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Experiment Configuration
+# -----------------------------------------------------------------------------
+
+# Basic Configuration
+# log_file_path = "logs/sim_hot_cold_rps18_1800.log"
+log_file_path = "logs/test_greedy_simulation.log"
+# model_name = "meta-llama/Llama-2-7b-hf"
+model_name = "mistralai/Mistral-7B-v0.1"
+exp_time = 100
+
+# GPU Configuration
+gpu_configs = [
+    GPUConfig(gpu_id=0, url=None, use_ssh=False),
+    GPUConfig(gpu_id=1, url=None, use_ssh=False),
+]
+add_simulation_to_gpu_config(gpu_configs)
+
+# for config in gpu_configs[2:]:
+#     config.regist_simulator_config(mistral_7b_A6000_sglang_tp(2.0), 75 << 30)
+
+# SGLang Runtime Configuration
+server_args = {
+    "model_path": model_name,
+    'gpu_configs': gpu_configs,
+    'log_prefix_hit': True,
+    'mem_fraction_static': 0.8,
+    'context_length': 2048,
+    "enable_flashinfer": True
+}
+
+# Workload Configuration
+configurations_to_test = [
+    [200, 0.2, 400, 4],
+]
+workload_configs = create_workload_prefix_configs(configurations_to_test, model_name, exp_time)
+
+# Selector Configuration
+# Format {policy - custom policy - message}
+selectors_configs = [
+    (DataParallelRuntimeSelectionPolicy.CUSTOM, CustomPolicyType.GREEDY_LP, 'greedy'),
+    (DataParallelRuntimeSelectionPolicy.RANDOM, "", "random"),
+]
+
+exp_args = MajorExperimentArgs(
+    server_args,
+    workload_configs,
+    gpu_configs,
+    simulate=False,
+    log_file_path=log_file_path,
+    selector_configs=selectors_configs,
+)
