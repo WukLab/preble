2765d2e2181738063c61969f0dedc26cd8652d2b
ben <dongmingli_Ben@126.com>
2024-04-27 23:09:29 +0000

    add 3 data loader

---
+++ multi_node/benchmarks/chameleon/__init__.py
@@ -0,0 +1,12 @@
+"""Prompts for the Chameleon TabWMP dataset."""
+
+from . import prompt_cl
+from . import prompt_kr
+from . import prompt_pg
+from . import prompt_policy
+from . import prompt_rl
+from . import prompt_sg
+from . import prompt_tv
+
+__all__ = ['prompt_cl', 'prompt_kr', 'prompt_pg', 'prompt_policy', 'prompt_rl',
+           'prompt_sg', 'prompt_tv']
